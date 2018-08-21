fffe9a5c07c454d19052343443b38742760c20a6
chhshi <chhshi1010@gmail.com>
2018-08-20 23:55:44 -0700

    @BeforeEach @AfterAll assertArrayEquals assertThrows assertTimeout

--- src/test/java/com/in28minutes/junit/helper/ArraysCompareTest.java
+++ src/test/java/com/in28minutes/junit/helper/ArraysCompareTest.java
@@ -9,33 +9,33 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class ArraysCompareTest {
 
-    @Test
-    public void testArraySort_RandomArray() {
-        int[] nums = {4, 3, 2, 1};
-        int[] numsSorted = {1, 2, 3, 4};
-        Arrays.sort(nums);
+  @Test
+  public void testArraySort_RandomArray() {
+    int[] nums = {4, 3, 2, 1};
+    int[] numsSorted = {1, 2, 3, 4};
+    Arrays.sort(nums);
 //        assertEquals(nums, numsSorted);  //compare is same object OR not
-        assertArrayEquals(nums, numsSorted);
-    }
+    assertArrayEquals(nums, numsSorted);
+  }
 
 
-    @Test
-    public void testArraySort_NullArray() {
-        int[] nums = null;
-        //same as @Test(expected=NullPointerException.class) in Junit4
-        assertThrows(NullPointerException.class, ()-> Arrays.sort(nums));
-    }
+  @Test
+  public void testArraySort_NullArray() {
+    int[] nums = null;
+    //same as @Test(expected=NullPointerException.class) in Junit4
+    assertThrows(NullPointerException.class, () -> Arrays.sort(nums));
+  }
 
-    @Test
-    public void testSort_Performace() {
-        //same as @Test(timeout=80) in Junit4
-        assertTimeout(Duration.ofMillis(80), () -> {
-            int[] arr = {12, 23, 4};
-            for (int i = 1; i < 1000000; i++) {
-                arr[0] = i;
-                Arrays.sort(arr);
-            }
-        });
-    }
+  @Test
+  public void testSort_Performace() {
+    //same as @Test(timeout=80) in Junit4
+    assertTimeout(Duration.ofMillis(80), () -> {
+      int[] arr = {12, 23, 4};
+      for (int i = 1; i < 1000000; i++) {
+        arr[0] = i;
+        Arrays.sort(arr);
+      }
+    });
+  }
 
 }
--- src/test/java/com/in28minutes/junit/helper/StringHelperTest.java
+++ src/test/java/com/in28minutes/junit/helper/StringHelperTest.java
@@ -9,59 +9,56 @@ import static org.junit.jupiter.api.Assertions.*;
 class StringHelperTest {
 
 
+  private static StringHelper sh1;
+  private StringHelper sh;
 
-    private static StringHelper sh1;
-    private StringHelper sh;
+  @BeforeAll  //must be static, same as @BeforeClass in Junit4
+  public static void beforeAll() {
+    sh1 = new StringHelper();
+    System.out.println("Before All . . . ");
+  }
 
-    @BeforeAll  //must be static, same as @BeforeClass in Junit4
-    public static void beforeAll() {
-        sh1 = new StringHelper();
-        System.out.println("Before All . . . ");
-    }
-
-    @AfterAll
-    public static void afterAll() {
-        System.out.println("After All . . .");
-    }
-
+  @AfterAll
+  public static void afterAll() {
+    System.out.println("After All . . .");
+  }
 
 
-    @BeforeEach  //must not be static, same as @Before in Junit4
-    public void before() {
-        sh = new StringHelper();
-        System.out.println("Before Each");
-    }
+  @BeforeEach  //must not be static, same as @Before in Junit4
+  public void before() {
+    sh = new StringHelper();
+    System.out.println("Before Each");
+  }
 
-    @AfterEach
-    public void after() {
-        System.out.println("After Each");
-    }
+  @AfterEach
+  public void after() {
+    System.out.println("After Each");
+  }
 
 
+  @Test
+  public void testTruncateAInFirst2Positions_aInFirst2Positions() {
+    assertEquals("CD", sh1.truncateAInFirst2Positions("AACD"));
+  }
 
-    @Test
-    public void testTruncateAInFirst2Positions_aInFirst2Positions() {
-        assertEquals("CD", sh1.truncateAInFirst2Positions("AACD"));
-    }
+  @Test
+  public void testTruncateAInFirst2Positions_aNotInFirst2Positions() {
+    assertEquals("CDAA", sh1.truncateAInFirst2Positions("CDAA"));
+  }
 
-    @Test
-    public void testTruncateAInFirst2Positions_aNotInFirst2Positions() {
-        assertEquals("CDAA", sh1.truncateAInFirst2Positions("CDAA"));
-    }
+  @Test
+  public void testAreFirstAndLastTwoCharactersTheSame_Same() {
+    assertTrue(sh.areFirstAndLastTwoCharactersTheSame("ABAB"));
+  }
 
-    @Test
-    public void testAreFirstAndLastTwoCharactersTheSame_Same() {
-        assertTrue(sh.areFirstAndLastTwoCharactersTheSame("ABAB"));
-    }
+  @Test
+  public void testAreFirstAndLastTwoCharactersTheSame_NotSame() {
+    assertFalse(sh.areFirstAndLastTwoCharactersTheSame("ABCD"));
+  }
 
-    @Test
-    public void testAreFirstAndLastTwoCharactersTheSame_NotSame() {
-        assertFalse(sh.areFirstAndLastTwoCharactersTheSame("ABCD"));
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = { "ABAB", "CDCD", "AAAA" })
-    public void testAreFirstAndLastTwoCharactersTheSame(String candidate) {
-        assertTrue(sh.areFirstAndLastTwoCharactersTheSame(candidate));
-    }
+  @ParameterizedTest
+  @ValueSource(strings = {"ABAB", "CDCD", "AAAA"})
+  public void testAreFirstAndLastTwoCharactersTheSame(String candidate) {
+    assertTrue(sh.areFirstAndLastTwoCharactersTheSame(candidate));
+  }
 }
